343b3c20ec4e8140aa973fa9df02f5a4533bc0c0
github-action-benchmark <github@users.noreply.github.com>
2022-11-02 01:39:18 +0000

    add SHME BenchmarkDotNet results (benchmarkdotnet) benchmark result for 34263dc87220ef7239953b10172f9129ac64fe6d

---
+++ SHME/bench/data.js
@@ -0,0 +1,56 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1667353158085,
+  "repoUrl": "https://github.com/ItEndsWithTens/SilentHillMapExaminer",
+  "entries": {
+    "SHME BenchmarkDotNet results": [
+      {
+        "commit": {
+          "author": {
+            "email": "robert.martens@gmail.com",
+            "name": "Rob Martens",
+            "username": "ItEndsWithTens"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "34263dc87220ef7239953b10172f9129ac64fe6d",
+          "message": "Try renaming the benchmark results again",
+          "timestamp": "2022-11-01T21:36:00-04:00",
+          "tree_id": "5736da8b38b5c14e1aa66801396a78d27f6568b7",
+          "url": "https://github.com/ItEndsWithTens/SilentHillMapExaminer/commit/34263dc87220ef7239953b10172f9129ac64fe6d"
+        },
+        "date": 1667353150334,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "SHME.Benchmarks.Graphics.AabbBenchmarks.AddAabb",
+            "value": 477.7604230244954,
+            "unit": "ns",
+            "range": "± 1.6527563287665912"
+          },
+          {
+            "name": "SHME.Benchmarks.Graphics.CameraBenchmarks.CannotSeeOffscreenRenderable",
+            "value": 255.76410929361978,
+            "unit": "ns",
+            "range": "± 0.9289240649233967"
+          },
+          {
+            "name": "SHME.Benchmarks.Graphics.CameraBenchmarks.ClipLineAgainstFrustum",
+            "value": 3859.7261555989585,
+            "unit": "ns",
+            "range": "± 8.50653717797136"
+          },
+          {
+            "name": "SHME.Benchmarks.Graphics.CameraBenchmarks.SetAngles",
+            "value": 7567.917809119592,
+            "unit": "ns",
+            "range": "± 16.03383887876737"
+          }
+        ]
+      }
+    ]
+  }
+}
